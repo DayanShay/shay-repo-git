c7bb973597b492e9c98aa2e8fb23691c309e741c
DayanShay <108628136+DayanShay@users.noreply.github.com>
2022-07-13 21:03:54 +0300

    Adding -f option
    The option "-f" is added to password_validator.py
    
    Now password_validator.py , can retrieved password from a text file
    Usage example:
    
    Python ./password_validator.py -f "/mypath/password.txt"

--- password_validator.py
+++ password_validator.py
@@ -20,9 +20,13 @@ def password_validator():
     (3) If a validation failed provide a message explaining why.
 
     How to use password_validator.py
+
         1) Run with CMD and send a "password":
             Commend line example -> : (python ./password_validator.py "Password1!")
             function Get password as "Password1!" (wright your "password" to check)
+
+        2) Run with CMD and ask to read "password" from a file with a file path:
+            Commend line example -> : (python ./password_validator.py -f "/mypath/password.txt")
 
         :return: sys.exit(code) 0 = valid ,1 = invalid.
         """
@@ -31,16 +35,37 @@ def password_validator():
     init(autoreset=True)
     red = "\033[31m"
     green = "\033[32m"
-    if len(sys.argv) == 2:
-        password_check = sys.argv[1]
-    else:
-        if len(sys.argv) > 2 :
-            print(red + "Password InValid.")
-            print("Got more then 1 arguments - can't run the Validation")
-            exit(1)
+    file_path = ""
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 3:
+            password_check = sys.argv[1]
+            if password_check == "-f":
+                try:
+                    file_path = sys.argv[2]
+                except IndexError:
+                    print(red + "Password InValid.")
+                    print("function -f sent with out a file Path")
+                    exit(1)
+                try:
+                    with open(file_path, "r") as file_path_password:
+                        password_check = file_path_password.read()
+                except OSError:
+                    print(red + "Password InValid.")
+                    print("invalid file Path : No such file or directory, Please check if file exist and path is correct")
+                    exit(1)
+            else:
+                print(red + "Password InValid.")
+                print(f"Function {password_check} Unrecognized")
+                exit(1)
         else:
-            password_check = ""
-
+            if len(sys.argv) == 2 :
+                password_check = sys.argv[1]
+            else:
+                print(red + "Password InValid.")
+                print("Got more then 1 arguments - can't run the Validation")
+                exit(1)
+    else:
+        password_check =""
     length_flag = len(password_check) > 9
     latter_flag = False
     number_flag = False
@@ -56,20 +81,20 @@ def password_validator():
                     capital_flag = True
                 else:
                     lower_flag = True
-    if length_flag and latter_flag and number_flag and lower_flag and capital_flag:
+    if length_flag and number_flag and lower_flag and capital_flag:
         print(green + "Password Valid.")
         return exit(0)
     else:
         print(red + "Password InValid.")
         counter_msg = 1
     if not length_flag:
-        print("("+str(counter_msg)+")", "Password too short - Password must have Minimum of 10 characters")
+        print("("+str(counter_msg)+")", "Password too short - Password must be Minimum of 10 characters")
         counter_msg += 1
-    if not latter_flag or not number_flag:
-        print("("+str(counter_msg)+")", "Password must contain both alphabet and number")
+    if not number_flag or not latter_flag:
+        print("("+str(counter_msg)+")", "Password don't have both alphabet and number")
         counter_msg += 1
     if not capital_flag or not lower_flag:
-        print("("+str(counter_msg)+")", "Password must contain both small and capital case letters")
+        print("("+str(counter_msg)+")", "Password don't have both small and capital case letters")
     return exit(1)
 
 
